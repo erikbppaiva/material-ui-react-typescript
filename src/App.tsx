e4f4c0e43de089197f96490eaf1bc4ee87fe264e
Erik-Paiva <erikbppaiva@gmail.com>
2022-10-11 16:48:47 -0300

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./routes";
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
